5ef421e93858b060865cd7d445b2464384b4db8a
Cristian Ștefan <98966871+CristiiiSE@users.noreply.github.com>
2022-04-23 13:58:13 +0200

    Add files via upload

---
+++ Cristi Shutdown Utility.bat
@@ -0,0 +1,6 @@
+@echo off
+color 03
+title Cristi Shutdown Utility by Cristian Stefan
+echo More info on my Github!
+shutdown -i
+exit
---
+++ Wi-Fi Cracker CMG.bat
@@ -0,0 +1,9 @@
+@echo off
+color 03
+title WiFi Cracker by Cristian Stefan
+echo WiFi Stats Loading.
+SLEEP 2
+echo WiFi Stats Loading..
+echo WiFi Stats Loading...
+netsh wlan show profile Proximus-Home-5388 key=clear
+pause
